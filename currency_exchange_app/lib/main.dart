699f23fd341904265296fe433363a905715dfcef
arsen <arsen9354@gmail.com>
2025-06-20 11:33:03 +0500

    добавил апишку теперь подтягивает и выводит норм использую https://open.er-api.com/v6/latest/

--- currency_exchange_app/lib/main.dart
+++ currency_exchange_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Currenввуcy Exchange',
+      title: 'Currency Exchange',
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
       ),
--- currency_exchange_app/lib/screens/home_screen.dart
+++ currency_exchange_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/currency_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,15 +13,20 @@ class _HomeScreenState extends State<HomeScreen> {
   String toCurrency = 'EUR';
   double amount = 0;
   double result = 0;
+  final CurrencyService _currencyService = CurrencyService();
 
   final List<String> currencies = ['USD', 'EUR', 'GBP', 'TRY'];
 
   final TextEditingController _controller = TextEditingController();
 
-  void convert() {
+  void convert() async{
     // Примитивная конвертация для примера
+    
+    final rate = await _currencyService.fetchExchangeRate(fromCurrency, toCurrency);
+    print('API rate: $rate');
     setState(() {
-      result = amount * 0.9; // Просто пример, позже заменим на API
+      //result = amount * 0.9; // Просто пример, позже заменим на API
+      result = rate != null ? amount * rate : 0;
     });
   }
 
@@ -31,37 +37,49 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Currency Exchange'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(30.0),
         child: Column(
           children: [
-            DropdownButton<String>(
-              value: fromCurrency,
-              onChanged: (value) {
-                setState(() {
-                  fromCurrency = value!;
-                });
-              },
-              items: currencies
-                  .map((currency) => DropdownMenuItem(
-                        value: currency,
-                        child: Text(currency),
-                      ))
-                  .toList(),
-            ),
-            DropdownButton<String>(
-              value: toCurrency,
-              onChanged: (value) {
-                setState(() {
-                  toCurrency = value!;
-                });
-              },
-              items: currencies
-                  .map((currency) => DropdownMenuItem(
-                        value: currency,
-                        child: Text(currency),
-                      ))
-                  .toList(),
-            ),
+   Row(
+    children: [
+      Expanded(
+        child: DropdownButton<String>(
+          value: fromCurrency,
+          onChanged: (value) {
+            setState(() {
+              fromCurrency = value!;
+            });
+          },
+          items: currencies
+              .map((currency) => DropdownMenuItem(
+                    value: currency,
+                    child: Text(currency),
+                  ))
+              .toList(),
+        ),
+      ),
+      const SizedBox(width: 16),
+      Expanded(
+        child: DropdownButton<String>(
+          value: toCurrency,
+          onChanged: (value) {
+            setState(() {
+              toCurrency = value!;
+            });
+          },
+          items: currencies
+              .map((currency) => DropdownMenuItem(
+                    value: currency,
+                    child: Text(currency),
+                  ))
+              .toList(),
+        ),
+      ),
+    ],
+  ),
+
+            const SizedBox(height: 50),
+            
             TextField(
               controller: _controller,
               keyboardType: TextInputType.number,
