9c62379186bc06572f3fce941fbbfa8090ded798
arsen <arsen9354@gmail.com>
2025-06-20 08:41:42 +0500

    addded a homescreen basic design for my app

--- currency_exchange_app/lib/main.dart
+++ currency_exchange_app/lib/main.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key:key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  String fromCurrency = 'USD';
+  String toCurrency = 'EUR';
+  double amount = 1.0;
+  double result = 0.0;
+
+  final List<String> currencies = ['USD', 'EUR','GBR', 'TRY'];
+
+  final TextEditingController _controller = TextEditingController();
+
+  void convert() {
+    //примитивная тестовая конвертация
+    setState(() {
+      result = amount * 0.9;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Currency Exchange'),
         ),
-      ),
+        body:  Padding(
+          padding: const EdgeInsets.all( 16.0),
+          child: Column(
+            children: [
+              DropdownButton<String>(
+                value: fromCurrency,
+                onChanged: (value){
+                  setState(() {
+                  toCurrency = value!;
+                  });
+                },
+                items: currencies
+                .map((currency) => DropdownMenuItem(
+                  value: currency,
+                  child: Text(currency),
+                ))
+                .toList()
+              ),
+              TextField(
+                controller: _controller,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Amount'),
+                onChanged:(value){
+                  amount = double.tryParse(value) ?? 0.0;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: convert, 
+                child: const Text('convert'),
+              ),
+              const SizedBox(height: 16.0),
+              Text('Result: $result')
+            ]
+          )
+        ),
     );
   }
 }
